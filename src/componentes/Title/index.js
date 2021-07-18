663326b2538af74c58f2e0766648039b5742d542
Renata Pulz <84743054+renatapulz@users.noreply.github.com>
2021-07-18 00:40:24 -0300

    criando aplicativo em react native

---
+++ src/componentes/Title/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+
+export default function Title(){
+    return(
+        <View>
+            <Text>
+                CÁLCULO DO IMC
+            </Text>
+        </View>
+    )
+}
