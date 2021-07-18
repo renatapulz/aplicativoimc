47e3b9ddb0211c889d69e49d8cbe8155a173a4c0
Renata Pulz <84743054+renatapulz@users.noreply.github.com>
2021-07-18 17:01:02 -0300

    concluido app

--- src/componentes/Form/ResultImc/index.js
+++ src/componentes/Form/ResultImc/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { View, Text } from "react-native"
+import styles from "./style"
 
 
 export default function ResultImc(props){
     return(
-        <View>
-            <Text>{props.messageResultImc}</Text>
-            <Text>{props.resultImc}</Text>
+        <View style={styles.ResultImc}>
+            <Text style={styles.information}>{props.messageResultImc}</Text>
+            <Text style={styles.numberImc}>{props.resultImc}</Text>
         </View>
     )
 }
