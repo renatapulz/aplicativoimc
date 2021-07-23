0c7f5410ab60caa7de9d47f0cf6853743bcb0318
Renata Pulz <84743054+renatapulz@users.noreply.github.com>
2021-07-23 17:43:21 -0300

    alterado conteudo

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './src/componentes/Title/';
-import Main from './src/componentes/Main/';
+import Form from './src/componentes/Form/';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title/>
-      <Main/>
+      <Form/>
     </View>
   );
 }
